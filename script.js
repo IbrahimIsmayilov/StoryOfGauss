00fcd2c2f3d8d1d8502ab439ffa488aee4aa7bfb
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-16 14:42:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Story of Gauss by Ibrahim Ismayilov
+
+// HTML Elements
+let calcBtn = document.getElementById("calc-btn"); 
+
+
+//   Add Event Listener
+calcBtn.addEventListener("click", calcSum);  
